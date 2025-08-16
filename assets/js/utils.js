7d5f1136dc9526a617bbe15461dd643fbbd73e30
alltasks24th <all.tasks.24.th@gmail.com>
2025-08-16 15:29:13 +0700

    Delete assets/js/utils.js

--- assets/js/utils.js
+++
@@ -1,4 +0,0 @@
-// assets/js/utils.js
-export const el = (sel, parent=document) => parent.querySelector(sel);
-export const els = (sel, parent=document) => Array.from(parent.querySelectorAll(sel));
-export const fmtDate = d => new Date(d).toLocaleString('th-TH');
